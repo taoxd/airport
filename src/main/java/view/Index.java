4a3e5b2f89f0d0d3340b841a36641b74c394faa3
taoxudong <taoxudong@oraro.net>
2019-06-25 17:48:37 +0800

    新增首页
    Change-Id: I16537ba2413e60bb10cdc436de8c1eb93b7e993c

---
+++ src/main/java/view/Index.java
@@ -0,0 +1,172 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.JSlider;
+import javax.swing.JInternalFrame;
+
+public class Index extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Index frame = new Index();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Index() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 977, 778);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setForeground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JLabel lblXml = new JLabel("XML命名:");
+		lblXml.setFont(new Font("宋体", Font.PLAIN, 16));
+		lblXml.setBounds(239, 44, 81, 15);
+		contentPane.add(lblXml);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("宋体", Font.PLAIN, 16));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"登机广播"}));
+		comboBox.setBounds(318, 36, 105, 31);
+		contentPane.add(comboBox);
+		
+		JLabel lblchn = new JLabel("广播词中文名-Chn:");
+		lblchn.setFont(new Font("宋体", Font.PLAIN, 16));
+		lblchn.setBounds(167, 105, 142, 15);
+		contentPane.add(lblchn);
+		
+		textField = new JTextField();
+		textField.setBounds(318, 98, 105, 31);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("新增类别");
+		btnNewButton.setFont(new Font("宋体", Font.PLAIN, 16));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(30, 144, 255));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(564, 37, 104, 31);
+		contentPane.add(btnNewButton);
+		
+		JLabel lbleng = new JLabel("英文别名-Eng:");
+		lbleng.setFont(new Font("宋体", Font.PLAIN, 16));
+		lbleng.setBounds(449, 105, 105, 15);
+		contentPane.add(lbleng);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(563, 98, 105, 31);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel label = new JLabel("语种选择:");
+		label.setFont(new Font("宋体", Font.PLAIN, 16));
+		label.setBounds(234, 169, 72, 15);
+		contentPane.add(label);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setFont(new Font("宋体", Font.PLAIN, 16));
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Chn"}));
+		comboBox_1.setBounds(318, 161, 225, 31);
+		contentPane.add(comboBox_1);
+		
+		JLabel label_1 = new JLabel("固定广播词:");
+		label_1.setFont(new Font("宋体", Font.PLAIN, 16));
+		label_1.setBounds(217, 231, 88, 31);
+		contentPane.add(label_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(SystemColor.control);
+		textArea.setForeground(new Color(0, 0, 0));
+		textArea.setFont(new Font("宋体", Font.PLAIN, 16));
+		textArea.setLineWrap(true);
+		textArea.setBounds(318, 235, 340, 192);
+		contentPane.add(textArea);
+		
+		JLabel label_2 = new JLabel("可变广告词:");
+		label_2.setFont(new Font("宋体", Font.PLAIN, 16));
+		label_2.setBounds(216, 464, 93, 31);
+		contentPane.add(label_2);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setFont(new Font("宋体", Font.PLAIN, 16));
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"城市"}));
+		comboBox_2.setBounds(318, 464, 225, 31);
+		contentPane.add(comboBox_2);
+		
+		JLabel label_3 = new JLabel("模版内容:");
+		label_3.setFont(new Font("宋体", Font.PLAIN, 16));
+		label_3.setBounds(232, 534, 72, 25);
+		contentPane.add(label_3);
+		
+		textField_2 = new JTextField();
+		textField_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField_2.setEditable(false);
+		textField_2.setFont(new Font("宋体", Font.PLAIN, 16));
+		textField_2.setBounds(317, 534, 340, 86);
+		contentPane.add(textField_2);
+		textField_2.setColumns(20);
+		
+		JButton btnNewButton_1 = new JButton("重置");
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setBackground(new Color(30, 144, 255));
+		btnNewButton_1.setFont(new Font("宋体", Font.PLAIN, 16));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(316, 651, 93, 31);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("提交");
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2.setBackground(new Color(30, 144, 255));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setFont(new Font("宋体", Font.PLAIN, 16));
+		btnNewButton_2.setBounds(564, 651, 93, 31);
+		contentPane.add(btnNewButton_2);
+	}
+}
--- src/main/java/view/JFrameRun.java
+++ src/main/java/view/JFrameRun.java
@@ -1,12 +1,19 @@
 package view;
 
 import config.Constant;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  * @Description: 添加常量广播词
@@ -17,8 +24,6 @@ import java.awt.event.ActionListener;
 public class JFrameRun extends JFrame {
 
     private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JTextField textField_2;
 
     /**
      * Launch the application.
@@ -42,75 +47,116 @@ public class JFrameRun extends JFrame {
     public JFrameRun() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 657, 331);
-        contentPane = new JPanel();
+        JPanel contentPane = new JPanel();
         contentPane.setForeground(Color.BLACK);
         contentPane.setBackground(Color.WHITE);
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(null);
         setContentPane(contentPane);
 
+        JLabel languageLabel = new JLabel("语种:");
+        languageLabel.setFont(new Font("宋体", Font.PLAIN, 16));
+        languageLabel.setBounds(38, 49, 54, 15);
+        contentPane.add(languageLabel);
+
         //语种下拉框
-        JComboBox comboBox = new JComboBox();
-        comboBox.setModel(new DefaultComboBoxModel(new String[]{"Chn"}));
-        comboBox.setFont(new Font("宋体", Font.PLAIN, 24));
-        comboBox.setToolTipText("1");
-        comboBox.setBounds(89, 36, 91, 36);
-        contentPane.add(comboBox);
+        JComboBox<String> languageComboBox = new JComboBox<String>();
+        languageComboBox.setModel(new DefaultComboBoxModel(new String[]{"Chn"}));
+        languageComboBox.setFont(new Font("宋体", Font.PLAIN, 24));
+        languageComboBox.setToolTipText("1");
+        languageComboBox.setBounds(89, 36, 91, 36);
+        contentPane.add(languageComboBox);
+
+        //资源
+        JLabel resourceLabel = new JLabel("资源:");
+        resourceLabel.setFont(new Font("宋体", Font.PLAIN, 16));
+        resourceLabel.setBounds(38, 109, 54, 15);
+        contentPane.add(resourceLabel);
 
         //资源名称
-        textField_2 = new JTextField();
-        textField_2.setBounds(89, 101, 385, 36);
-        contentPane.add(textField_2);
-        textField_2.setColumns(10);
-
+        JTextField resourceTextField = new JTextField();
+        resourceTextField.setBounds(89, 101, 385, 36);
+        contentPane.add(resourceTextField);
+        resourceTextField.setColumns(10);
 
         //导入音频显示文件名
-        JLabel lblBoardmp = new JLabel();
-        lblBoardmp.setBounds(89, 149, 299, 15);
-        contentPane.add(lblBoardmp);
+        JLabel audioName = new JLabel();
+        audioName.setBounds(89, 149, 299, 15);
+        contentPane.add(audioName);
 
-        JButton btnNewButton = new JButton("导入音频");
-        btnNewButton.addActionListener(new ActionListener() {
+        JButton audioButton = new JButton("导入音频");
+        audioButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fc = new JFileChooser(Constant.IMPORT_VOICE_OPEN_URL);
                 int val = fc.showOpenDialog(null);    //文件打开对话框
                 if (val == fc.APPROVE_OPTION) {
                     //正常选择文件
-                    lblBoardmp.setText(fc.getSelectedFile().toString());
+                    audioName.setText(fc.getSelectedFile().toString());
                 } else {
                     //未正常选择文件，如选择取消按钮
-                    lblBoardmp.setText("未选择文件");
+                    audioName.setText("未选择文件");
                 }
             }
         });
 
-        btnNewButton.setFont(new Font("宋体", Font.PLAIN, 12));
-        btnNewButton.setBackground(new Color(30, 144, 255));
-        btnNewButton.setForeground(new Color(240, 248, 255));
-        btnNewButton.setBounds(503, 104, 91, 28);
-        contentPane.add(btnNewButton);
+        audioButton.setFont(new Font("宋体", Font.PLAIN, 12));
+        audioButton.setBackground(new Color(30, 144, 255));
+        audioButton.setForeground(new Color(240, 248, 255));
+        audioButton.setBounds(503, 104, 91, 28);
+        contentPane.add(audioButton);
 
-        JLabel label = new JLabel("语种:");
-        label.setFont(new Font("宋体", Font.PLAIN, 16));
-        label.setBounds(38, 49, 54, 15);
-        contentPane.add(label);
+        JButton submitButton = new JButton("提交");
+        submitButton.setForeground(new Color(255, 255, 255));
+        submitButton.setBackground(new Color(30, 144, 255));
+        submitButton.setFont(new Font("宋体", Font.PLAIN, 16));
+        submitButton.setBounds(89, 174, 93, 36);
+        contentPane.add(submitButton);
 
-        JLabel label_1 = new JLabel("资源:");
-        label_1.setFont(new Font("宋体", Font.PLAIN, 16));
-        label_1.setBounds(38, 109, 54, 15);
-        contentPane.add(label_1);
-
-        JButton button = new JButton("提交");
-        button.setForeground(new Color(255, 255, 255));
-        button.setBackground(new Color(30, 144, 255));
-        button.setFont(new Font("宋体", Font.PLAIN, 16));
-        button.setBounds(89, 174, 93, 36);
-        contentPane.add(button);
-
-        button.addActionListener(new ActionListener() {
+        submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println(112233);
+                System.out.println(languageComboBox.getSelectedIndex() + "," + languageComboBox.getSelectedItem());
+                System.out.println(resourceTextField.getText());
+                System.out.println(audioName.getText());
+                writeXML();
             }
         });
+    }
+
+    public void writeXML() {
+        Document document = DocumentHelper.createDocument();
+        Element rootElement = document.addElement("resources");
+        Element actionElement1 = rootElement.addElement("resourceType");
+        actionElement1.addAttribute("typeId", "100");
+        Element forwardElement1 = actionElement1.addElement("hashValue");
+        forwardElement1.addAttribute("id", "111");
+        Element forwardElement2 = actionElement1.addElement("resource");
+        forwardElement2.addAttribute("language", "chn");
+        forwardElement2.addAttribute("value", "fail.jsp");
+        forwardElement2.addAttribute("url", "fail.jsp");
+        forwardElement2.addAttribute("hashId", "11111");
+        try {
+            /**
+             *  指定文本的写出的格式：
+             *      紧凑格式 ：项目上线时候使用
+             *      漂亮格式：开发调试的时候使用
+             */
+            //OutputFormat format=OutputFormat.createCompactFormat();  //紧凑格式:去除空格换行
+            OutputFormat format = OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+            /**
+             * 指定生成的xml文档的编码影响了：
+             *         1.xml文档保存时的编码
+             *         2.xml文档声明的encoding编码（Xml解析时的编码）
+             * 结论：使用该方法生成Xml文档可以避免中文乱码问题
+             */
+            format.setEncoding("UTF-8");
+            /** 将document中的内容写入文件中 */
+            XMLWriter writer = new XMLWriter(new FileWriter(new File(
+                    "f://resource.xml")), format);
+            writer.write(document);
+            writer.close();
+            /** 执行成功,需返回1 */
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
